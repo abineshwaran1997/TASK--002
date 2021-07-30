0a5d0ecee05a1c00279d9822b25a32ead11c09dd
abineshwaran1997 <65437712+abineshwaran1997@users.noreply.github.com>
2021-07-30 12:04:34 +0530

    Add files via upload

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,15 +1,35 @@
 var request=new XMLHttpRequest();
-//2.opening a connection(specify the URL)
+
 request.open('GET','https://restcountries.eu/rest/v2/all',true);
-//3.sending a connection 
+
 request.send();
-//4.once the server responded successfully then we have to process the data
+
 request.onload=function(){
     var data=JSON.parse(this.response);
-    console.log(data);
-    for(var i=0;i<250;i++)
-    {
-        console.log(data[i].name+" "+data[i].capital);
-        console.log(data[i].flag)
-    }
+   console.log(data);
+
+   var rdata=data.filter((ele)=>ele.region==='Asia');
+   console.log(rdata);
+
+   var rdata1=data.filter((ele)=>ele.population<200000);
+   console.log(rdata1);
+
+   var rdata2=data.filter((ele)=>{
+       for(let i in ele.currencies){
+           if(ele.currencies[i].code==='USD'){
+               return true;
+           }
+       }
+   });
+   console.log(rdata2);
+   
+function rdata3(data){
+    const reduc=(rdata3,ele)=>rdata3+ele.population;
+    const intialv=0;
+    return data.reduce(reduc,intialv);
+}
+   
+  console.log( rdata3(data))
+
+    
 }
